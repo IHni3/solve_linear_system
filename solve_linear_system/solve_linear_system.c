ea6bd9958c8f478e4dd0d88c94dc8380bcd9726e
Aaron Joshua Beglau Awesome <aaronjoshua.beglau@student.schwarz>
2020-03-15 09:23:50 +0100

    added comments and TODOs

--- solve_linear_system/solve_linear_system.c
+++ solve_linear_system/solve_linear_system.c
@@ -5,16 +5,36 @@
 #include <string.h>
 #include <stdarg.h>
 #include "linear_system_functions.h"
-
 //#include "test.h"
 
-char* input(const char* text, const char* errortext, char* buffer,const size_t size, bool (*inputIsValid)(const char* c, void* returnValue, int, ...), int num, ...)
+
+/*  
+*Function: input
+*--------------------
+* Validates all inputs and returns a buffer (minimum size of path).
+* User does have unlimited amount of attempts to provide valid inputs.
+*
+* Arguments
+*			 text:			Output to guide the user (indicating the path).
+*			 errortext:		Output if an error occurs.
+*			 buffer:		Contains the size of the buffer.
+*			 size:			Fixed integer size of 512.
+*			 inputIsValid:	Consortium of variables that must be true 
+*							for the buffer size to be returned.
+*			 num:			Generic integer containing starting vectors.
+*
+* Returns
+*			 1. buffer (minimum size of path), when all variables have been validated.
+*/
+char* input(const char* text, const char* errortext, char* buffer, const size_t size, bool (*inputIsValid)(const char* c, void* returnValue, int, ...), int num, ...)
 {
 	bool bRet = false;
 	do
 	{
+
 		printf("%s",text);
-		fgets(buffer, size, stdin); //TODO problem wenn mehr als size
+		//TODO problem wenn mehr als size
+		fgets(buffer, size, stdin);
 		strtok(buffer, "\n");
 
 		va_list args;
@@ -26,15 +46,55 @@ char* input(const char* text, const char* errortext, char* buffer,const size_t s
 		{
 			printf("%s", errortext);
 		}
+
 	} while (!bRet);
 
 	return buffer;
 }
 
+
+/*  
+*Function: inputPath
+*--------------------
+* Acts as a wrapper and calls method 'load'; returns the boolean outcome.
+*
+* Arguments
+*			 c:				 Path to specified file.
+*			 pMatrix:		 A matrix with n rows and n columns.
+*			 pStartVector:	 Contains initial coefficients and iteration vectors.
+*			 pResultsVector: Contains resulting coefficients and iteration vectors.
+*
+* Returns
+*			 1. boolean value 'true', when all variables have been validated.
+*			 2. boolean value 'false' otherwise.
+*/
 bool inputPath(const char* c, Matrix* pMatrix, Vector* pStartVector, Vector* pResultsVector)
 {
 	return load(c, pMatrix, pStartVector, pResultsVector);
 }
+
+
+/*  
+*Function: inputMode
+*--------------------	
+* Returns the method to be used for solving
+* the linear system of equations based on the 
+* user input.
+*
+* Arguments
+*			 c:				variable acting as a logic switch.
+*
+*			 returnValue:	all available ENUM values within
+*							the ENUM 'Method'.
+*			 
+*
+* Returns
+*			1: boolean value 'true' and ENUM value 'JACOBI',
+*			   if input (c) is 1.
+*			2: boolean value 'true' and ENUM value 'GAUSS_SEIDEL,
+*			   if input (c) is 2.
+*			3: boolean value 'false' otherwise.
+*/
 bool inputMode(const char* c, Method* returnValue)
 {
 	if (strcmp(c, "1") == 0)
@@ -53,6 +113,19 @@ bool inputMode(const char* c, Method* returnValue)
 	}
 }
 
+
+/*  
+*Function: fileExists
+*--------------------	
+* Checks the accessibility of a given file.
+*
+* Arguments
+*			 filename:		Path to specified file.
+*
+* Returns
+*			 1. boolean value 'true', if the file exists.
+*			 2. boolean value 'false' otherwise.
+*/
 bool fileExists(const char* filename)
 {
 	FILE* file = fopen(filename, "r");
@@ -64,11 +137,32 @@ bool fileExists(const char* filename)
 	return false;
 }
 
+
+/*
+*Function: cleanInputBuffer
+*--------------------	
+* keine Referenzierung
+*/
+//TODO würde in clearInputBuffer umbenennen, falls die Funktion drin bleibt
 void cleanInputBuffer()
 {
 	while(getchar() != EOF);
 }
 
+
+/*  
+*Function: main
+*--------------------	
+* Main function which calls sub-functions.
+* Possesses the logic to handle fundamental method calls.
+*
+* Arguments
+*			 numberOfArguments:		Length of the argument vector.
+*			 argv:					Array of character pointers.
+*
+* Returns
+*			 None.
+*/
 int main(const int numberOfArguments, char** argv)
 {
 	//testAll();
@@ -92,7 +186,7 @@ int main(const int numberOfArguments, char** argv)
 		float timeDiff = stopStopwatch(t1);
 
 		printf("time: %.2fs\n", timeDiff);
-		printf("this program solves linear systems with jacobi or gauss-seiles algorithm\n\n");
+		printf("this program solves linear systems of equations with jacobi or gauss-seidel algorithm\n\n");
 
 		char* path = malloc(sizeof(char) * 512);
 		input("please enter file path of linear system (csv): ",
