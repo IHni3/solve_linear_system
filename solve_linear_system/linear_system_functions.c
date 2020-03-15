61ba39c36b1676cf0d62de86cc617c7beeb342d4
Aaron Joshua Beglau Awesome <aaronjoshua.beglau@student.schwarz>
2020-03-15 13:13:12 +0100

    added comments and TODOs 20200315

--- solve_linear_system/linear_system_functions.c
+++ solve_linear_system/linear_system_functions.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 #include <math.h>
 
+
+/*  
+*Function: currentFieldToNumber
+*--------------------
+* Takes a single field of type <char pointer> and returns
+* a boolean after validating the numerical correctness by
+* cycling through every character checking with isdigit().
+*
+* Arguments
+*			 field:			Char pointer containing a string consisting of 
+*							a field of the linear system of equations.
+*			 nSize:			Size of the specified field (single chars count, except dots).
+*			 result:		Double pointer as unique identifier of 
+*							a to double converted string.
+*
+* Returns
+*			 1. boolean value 'true', when all variables have been validated.
+*			 2. boolean value 'false' otherwise.
+*/
 bool currentFieldToNumber(const char* field, const int nSize, double* result)
 {
 	bool bRet = true;
@@ -23,13 +42,12 @@ bool currentFieldToNumber(const char* field, const int nSize, double* result)
 			}
 
 			double num = atof(str);
-
 			*result = num;
 		}
 
 		free(str);
-
 	}
+
 	else
 	{
 		bRet = false;
@@ -37,6 +55,22 @@ bool currentFieldToNumber(const char* field, const int nSize, double* result)
 
 	return bRet;
 }
+
+
+/*  
+*Function: initVector
+*--------------------
+* Initializes a vector as a function of the size of the vector.
+*
+* Arguments
+*			 vector:		A vector array with supposedly multiple entries.
+*			 nSize:			Size of the specified vector (single chars count, except dots).
+*
+* Returns
+*			 1. boolean value 'true', when all variables have been validated 
+*				and thus the vector was initialized successfully.
+*			 2. boolean value 'false' otherwise.
+*/
 bool initVector(Vector* vector, const int nSize)
 {
 	bool bRet = false;
@@ -55,6 +89,23 @@ bool initVector(Vector* vector, const int nSize)
 
 	return bRet;
 }
+
+
+/*  
+*Function: initMatrix
+*--------------------
+* Initializes a matrix as a function of the size of the matrix.
+*
+* Arguments
+*			 pMatrix:		A two-dimensional Array consisting of n rows and n lines.
+*			 nSize:			Size of the specified vector (single chars count, except dots).
+*							Should be the same for lines and rows (logically).
+*
+* Returns
+*			 1. boolean value 'true', when all variables have been validated 
+*				and thus the matrix was initialized successfully.
+*			 2. boolean value 'false' otherwise.
+*/
 bool initMatrix(Matrix* pMatrix, const int nSize)
 {
 	bool bRet = true;
@@ -101,6 +152,24 @@ bool initMatrix(Matrix* pMatrix, const int nSize)
 
 	return bRet;
 }
+
+
+/*  
+*Function: getDimensionsFromFile
+*--------------------
+* Evaluates the actual size of the file containing the 
+* given values for the linear system of equations.
+*
+* Arguments
+*			 cFilename:		Name of the file
+*			 nRows:			Amount of rows of given file
+*			 nCols:			Amount of lines of given file
+*
+* Returns
+*			 1. boolean value 'true', when all variables have been validated 
+*				and the correct size of vertical and horizontal 
+*			 2. boolean value 'false' otherwise.
+*/
 bool getDimensionsFromFile(const char* cFilename, int* nRows, int* nCols)
 {
 	bool bRet = false;
@@ -154,6 +223,29 @@ bool getDimensionsFromFile(const char* cFilename, int* nRows, int* nCols)
 	return bRet;
 }
 
+
+/*  
+*Function: interpretateDimensions
+*--------------------
+* Initializes a vector as a function of the size of the vector. Evaluates if either the
+* linear system of equations contains either a valid amount of starting vectors, 
+* a valid amount of reasonable results, both of which or ultimately none of both.
+*
+* Arguments
+*			 nRows:			Integer containing he amount of lines.
+*			 nCols:			Integer containing the amount of columns.
+*			 nCoefficients:	All values representing the coefficients of the linear
+*							system of equations. These are tested for proportional 
+*							length differences (+1) by this method.
+*			 bResults:		Boolean value depending on the presence of 
+*							already predefined results of the linear system of equations.
+*			 bStartVector:	Boolean value depending on the presence of starting vectors.
+*
+* Returns
+*			 1. boolean value 'true', when all variables have been validated 
+*				and one of the specifications matches. 
+*			 2. boolean value 'false' otherwise.
+*/
 bool interpretateDimensions(const int nRows, const int nCols, int* nCoefficients, bool* bResults, bool* bStartVector)
 {
 	bool bRet = false;
@@ -164,6 +256,7 @@ bool interpretateDimensions(const int nRows, const int nCols, int* nCoefficients
 		{
 			*nCoefficients = nRows;
 
+			//TODO woher weiß man, dass es nicht den Fall gibt, dass Startvektoren aber keine Lösungen gegeben sind?
 			if (nRows == nCols)
 			{
 				*bResults = false;
@@ -187,6 +280,25 @@ bool interpretateDimensions(const int nRows, const int nCols, int* nCoefficients
 	return bRet;
 }
 
+/*  
+*Function: insertMatrix
+*--------------------
+* Acts as a wrapper for the insertion of the matrix.
+* Automatically converts entry in field of matrix to digits 
+* (via sub-function) and fills the field after validation.
+*
+* Arguments
+*			 matrix:		A two-dimensional Array consisting of n rows and n lines.
+*			 nRow:			Integer representing the actual position on behalf of the amount of lines.
+*			 nCol:			Integer representing the actual position on behalf of the amount of columns.
+*			 field:			The unedited field of the matrix returned by functions reading the raw file.
+*			 nSize:			Integer consisting of the length of the given field.
+*
+* Returns
+*			 1. boolean value 'true', when all variables have been validated 
+*				and the matrix has been successfully filled with verified digits only.
+*			 2. boolean value 'false' otherwise.
+*/
 bool insertMatrix(const Matrix* matrix, const int nRow, const int nCol, const char* field, const int nSize)
 {
 	double num = 0;
@@ -196,14 +308,34 @@ bool insertMatrix(const Matrix* matrix, const int nRow, const int nCol, const ch
 		matrix->data[nRow][nCol] = num;
 		//printf("matrix.data[%d][%d] = %lf\n", nRow, nCol, num);
 	}
+	//TODO für was das else? bRet = false? Hat dann bei Funktionsaufruf aber kein exception handling...?
 	else
 	{
-
+		
 	}
 
 	return bRet;
 }
 
+
+/*  
+*Function: insertVector
+*--------------------
+* Acts as a wrapper for the insertion of the vector.
+* Automatically converts entry in field of matrix to digits 
+* (via sub-function) and fills the field after validation.
+*
+* Arguments
+*			 vector:		A one-dimensional Array consisting of the values of the starting vector.
+*			 n:				Indication of the current line.
+*			 field:			The unedited field of the matrix returned by functions reading the raw file.
+*			 nSize:			Integer consisting of the length of the given field.
+*
+* Returns
+*			 1. boolean value 'true', when all variables have been validated 
+*				and the vector has been successfully filled with verified digits only.
+*			 2. boolean value 'false' otherwise.
+*/
 bool insertVector(const Vector* vector, const int n, const char* field, const int nSize)
 {
 	double num = 0;
@@ -215,12 +347,28 @@ bool insertVector(const Vector* vector, const int n, const char* field, const in
 	}
 	else
 	{
-
+		//TODO warum ist das else hier?
 	}
 
 	return bRet;
 }
 
+/*  
+*Function: readFile
+*--------------------
+* Reads the file and verifies all inputs with the sub-functions above.
+* Ulitmately initializes the overall linear system of equations.
+*
+* Arguments
+*			 cFilename:		 String containing the relative path to the file.
+*			 pMatrix:		 A two-dimensional Array consisting of n rows and n lines.
+*			 pResultsVector: Vector containing the results of the linear system of equations.
+*			 pStartVector:	 Vector containing the initial values of the linear system of equations.
+*
+* Returns
+*			 1. boolean value 'true', when the linear system of equations has been successfully initialized.
+*			 2. boolean value 'false' otherwise.
+*/
 bool readFile(const char* cFilename, Matrix* pMatrix, Vector* pResultsVector, Vector* pStartVector)
 {
 	bool bReturn = false;
@@ -229,8 +377,6 @@ bool readFile(const char* cFilename, Matrix* pMatrix, Vector* pResultsVector, Ve
 	{
 
 		unsigned long ulSize = 0;  // Input File size
-
-
 
 		int rows = 0, cols = 0;
 		getDimensionsFromFile(cFilename, &rows, &cols);
@@ -281,7 +427,7 @@ bool readFile(const char* cFilename, Matrix* pMatrix, Vector* pResultsVector, Ve
 				{
 					nFieldSize += 3;
 					char* field = (char*)realloc(currentField, nFieldSize);
-					currentField = field; //auf null ptr pr�fen
+					currentField = field; //auf null ptr pr�fen
 				}
 
 				if (c == delimiter || c == newline)
@@ -324,19 +470,50 @@ bool readFile(const char* cFilename, Matrix* pMatrix, Vector* pResultsVector, Ve
 	return bReturn;
 }
 
+/*  
+*Function: load
+*--------------------
+* Acts as a wrapper for the function 'readFile'.
+*
+* Arguments
+*			 cFilename:		String containing the relative path to the file.
+*			 A:				A two-dimensional Array consisting of n rows and n lines.
+*			 b:				Vector containing the results of the linear system of equations.
+*			 x:				Vector containing the initial values of the linear system of equations.
+*
+* Returns
+*			 1. The return of 'readFile'.
+*/
 bool load(const char* cfilename, Matrix* A, Vector* b, Vector* x)
 {
 	return readFile(cfilename, A, b, x);
 }
 
+//TODO entfernen oder hat diese Funktion später noch einen Nutzen?
 //void solve(Method method, Matrix* A, Vector* b, Vector* x, double e)
 //{
 //
 //}
 
-// return absolute differnce value of two inserst
+
+/*  
+*Function: vectorAbs
+*--------------------
+* Returns the absolute difference value of two inserts.
+*
+* Arguments
+*			 a:		 	First double value for the calculation.
+*			 b:		 	Second double value for the calculation.
+*			 nSize: 	Member of the matrix structure.
+*			 result:	The calculation result.
+*
+* Returns
+*			 1. boolean value 'true', when the result was calculated successfully.
+*			 2. boolean value 'false' otherwise.
+*/
 bool vectorAbs(const double* a, const double* b, const int nSize, double* result)
-{								//@Tim : Ich weiß das wir prüfen wollen, ob man überhaupt einen Betrag berechnen kann, aber wie bekommen wir den Wert dann hier raus?
+{				
+	//TODO @Tim : Ich weiß dass wir prüfen wollen, ob man überhaupt einen Betrag berechnen kann, aber wie bekommen wir den Wert dann hier raus?
 	if (a && b && result)
 	{
 		double value = 0;
@@ -353,6 +530,22 @@ bool vectorAbs(const double* a, const double* b, const int nSize, double* result
 	}
 }
 
+
+/*  
+*Function: solveJacobi
+*--------------------
+* Takes the matrix, resulting vectors and starting vectors as arguments.
+* Calculates the linear system equation based on the jacobi iteration method.
+*
+* Arguments
+*			 pa:		A matrix with n rows and n lines.
+*			 pb:		The starting vectors.
+*			 px: 		The resulting vectors.
+*			 acc:		The accuracy of the calculation.
+*
+* Returns
+*			 None.
+*/
 void solveJacobi(Matrix* pa, Vector* pb, Vector* px, double acc)
 {
 	const int n = pa->n;
@@ -403,7 +596,7 @@ void solveJacobi(Matrix* pa, Vector* pb, Vector* px, double acc)
 		}
 		double precision = 0;			//@Tim: siehe oben
 
-		vectorAbs(preX, px->data, n, &precision); //TODO: r�ckgabewert pr�fen
+		vectorAbs(preX, px->data, n, &precision); //TODO: rueckgabewert pruefen
 												   //@Tim : Was stellen wir jetzt mit dem Bool an?
 		printf("Iterations: %d, Precision: %.11f\n", iteration, precision);
 
@@ -414,6 +607,22 @@ void solveJacobi(Matrix* pa, Vector* pb, Vector* px, double acc)
 	}
 }
 
+
+/*  
+*Function: solveGauss
+*--------------------
+* Takes the matrix, resulting vectors and starting vectors as arguments.
+* Calculates the linear system equation based on the gauss-seidel iteration method.
+*
+* Arguments
+*			 pa:		A matrix with n rows and n lines.
+*			 pb:		The starting vectors.
+*			 px: 		The resulting vectors.
+*			 acc:		The accuracy of the calculation.
+*
+* Returns
+*			 None.
+*/
 void solveGauss(Matrix* pa, Vector* pb, Vector* px, double acc)
 {
 
@@ -490,11 +699,34 @@ void solveGauss(Matrix* pa, Vector* pb, Vector* px, double acc)
 	} while (!accReached && counter <= 100);
 }
 
+/*
+*Function: startStopwatch
+*--------------------
+* Returns a timestamp resembling the current time.
+*
+* Arguments
+*			 None.
+*
+* Returns
+*			 1. A timestamp resembling the current time.
+*/
 clock_t startStopwatch()
 {
 	return clock();
 }
 
+
+/*
+*Function: startStopwatch
+*--------------------
+* Returns the number of clock ticks elapsed since the program was launched.
+*
+* Arguments
+*			 c:			First timestamp (starting value) to calculate the difference.		
+*
+* Returns
+*			 1. The difference between the timestamps as a float.
+*/
 float stopStopwatch(const clock_t c)
 {
 	const clock_t t2 = clock();
